0f4218b907e03425dc519f75b03d02e713ee41a3
thetiagogil <talmeidagil@gmail.com>
2024-01-31 21:42:22 +0000

    small change to homepage scss

--- src/db/certifications.js
+++ src/db/certifications.js
@@ -4,7 +4,8 @@ export const certifications = [
     name: "React Native Course",
     place: "Udemy",
     date: "2024",
-    description: "This is the description for Project 1.",
-    techs: ["React Native"],
+    description: "React Native course with hands-on experience, building an app from the ground up that explored functional and class components; implemented React Context and Hooks; integrated REST APIs; implemented tab navigation; enabled image uploads; created swipeable carousels and facilitated external actions like email and phone calls.",
+    link: "https://www.udemy.com/course/build-mobile-apps-with-react-native-from-to-zero-to-expert/",
+    techs: ["React Native", "JavaScript"],
   },
 ];
--- src/db/education.js
+++ src/db/education.js
@@ -5,20 +5,30 @@ export const education = [
     place: "Colégio Guadalupe",
     date: "2002 — 2014",
     description: "High School Science and Technology course.",
+    link: "https://c-guadalupe.com/en/",
   },
   {
     id: "1",
     name: "Master Degree in Architecture",
     place: "Faculty of Architecture, University of Lisbon",
     date: "2014 — 2022",
-    description: "Dissertation with the title “Human centered architecture: Video games as inspiration for the development of architecture“.",
+    description:
+      "Graduated as a Master Architect from the Faculty of Architecture, specializing in architecture and urban design. Throughout my academic journey, I developed several architectural and urban projects, collaborating with other students on some of them. My dissertation, titled 'Human centered architecture: Video games as inspiration for the development of architecture', explored aligning architectural design with user needs, inspired by the user-centric approach found in most video game development.",
+    link: "https://www.fa.ulisboa.pt/index.php/en/",
+    techs: [
+      "AutoCAD",
+      "SketchUp",
+      "Adobe Photoshop",
+      "Microsoft Office",
+    ],
   },
   {
     id: "2",
     name: "Web Development Course",
     place: "Ironhack School",
     date: "2023",
-    description: "9 weeks intensive web development bootcamp.",
+    description: "Graduated from Ironhack's School 9-week intensive Web Development Bootcamp, where I gained expertise in crafting web applications. The program taught me the foundations of Full-Stack Development, equipping me with valuable skills and technologies to contribute effectively to designing diverse web projects.",
+    link: "https://www.ironhack.com",
     techs: [
       "HTML",
       "CSS",
