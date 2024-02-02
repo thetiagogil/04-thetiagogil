e005410ac0580ccf6f3edd10b6c307a9544e843c
thetiagogil <talmeidagil@gmail.com>
2024-02-02 15:41:22 +0000

    filters init

--- src/pages/timelinepage/TimelinePage.jsx
+++ src/pages/timelinepage/TimelinePage.jsx
@@ -7,8 +7,18 @@ import { dateSorter } from "../../components/DateSorter";
 import { Link } from "react-router-dom";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { useState } from "react";
 
 const TimelinePage = () => {
+  const [typesfilter, setTypesFilter] = useState({
+    projects: true,
+    experience: true,
+    education: true,
+    certifications: true,
+  });
+
+  const [techsFilter, setTechsFilter] = useState([]);
+
   // ARRAY WITH ALL THE DATA - ADDED TYPE TO DATA
   const dataWithType = (data, type) => {
     return data.map((array) => {
@@ -30,6 +40,49 @@ const TimelinePage = () => {
 
   // SORT ARRAY BY DATE
   const dataSorted = dateSorter(data);
+
+  // TECHS ARRAY
+  const techsArray = () => {
+    let allTechs = new Set();
+
+    dataSorted.forEach((data) => {
+      const techs = data.techs;
+
+      techs?.forEach((tech) => {
+        allTechs.add(tech);
+      });
+    });
+
+    return Array.from(allTechs);
+  };
+  techsArray();
+
+  // FILTER ARRAY BY TYPE AND TECHS
+  const handleTypeChange = (type) => {
+    setTypesFilter((filter) => ({
+      ...filter,
+      [type]: !filter[type], // change selected type to false
+    }));
+  };
+
+  const handleTechsChange = (tech) => {
+    setTechsFilter((filter) => ({
+      ...filter,
+      techs: filter.techs.includes(tech)
+        ? filter.techs.filter((otherTechs) => otherTechs !== tech) // filter out other techs
+        : [...filter.techs, tech],
+    }));
+  };
+
+  const dataFiltered = dataSorted.filter((element) => {
+    const types = typesfilter[element.type]; // changes the state for the selected type
+    const techs =
+      techsFilter.length === 0 ||
+      (element.techs &&
+        element.techs.some((tech) => techsFilter.includes(tech)));
+
+    return types && techs;
+  });
 
   // COLORS PER TYPE
   const getColorClass = (type, category) => {
@@ -53,13 +106,15 @@ const TimelinePage = () => {
           <h1>My timeline</h1>
         </section>
 
+        <section className="filters"></section>
+
         <table className="table">
           <thead>
             <tr>
               <th className="head-date">Date</th>
               <th className="head-name">Name</th>
               <th className="head-place">Place</th>
-              <th className="head-techs">Stack Tech</th>
+              <th className="head-techs">Tech Stack</th>
             </tr>
           </thead>
 
