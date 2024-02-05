5bb6e1e39c7d835b45b026374eda300c266e6a2b
thetiagogil <talmeidagil@gmail.com>
2024-02-05 19:18:09 +0000

    bug fix

--- src/db/certifications.js
+++ src/db/certifications.js
@@ -1,11 +1,22 @@
 export const certifications = [
   {
     id: "0",
+    name: "Agile",
+    place: "Udemy",
+    date: "2024 Jan",
+    description:
+      "Explored the most popular Agile Methodologies such as Scrum, Kanban, DSDM, Extreme Programming (XP) and Lean Software Development.",
+    link: "https://www.udemy.com/course/agile-project-management-certification/",
+    techs: ["Agile Methodologies", "Lean Principles", "Kanban", "Jira"],
+  },
+  /* {
+    id: "1",
     name: "React Native Course",
     place: "Udemy",
     date: "2024 Jan",
-    description: "React Native course with hands-on experience, building an app from the ground up that explored functional and class components; implemented React Context and Hooks; integrated REST APIs; implemented tab navigation; enabled image uploads; created swipeable carousels and facilitated external actions like email and phone calls.",
+    description:
+      "React Native course with hands-on experience, building an app from the ground up that explored functional and class components; implemented React Context and Hooks; integrated REST APIs; implemented tab navigation; enabled image uploads; created swipeable carousels and facilitated external actions like email and phone calls.",
     link: "https://www.udemy.com/course/build-mobile-apps-with-react-native-from-to-zero-to-expert/",
-    techs: ["React Native", "JavaScript"],
-  },
+    techs: ["React Native", "JavaScript", "REST API"],
+  }, */
 ];
