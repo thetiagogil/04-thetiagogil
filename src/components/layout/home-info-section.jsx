a64bf92f5a008063fe9dbc2d46e12e8d39f458b6
thetiagogil <talmeidagil@hotmail.com>
2024-09-27 16:47:57 +0100

    add download resume to home page

--- src/components/layout/home-info-section.jsx
+++ src/components/layout/home-info-section.jsx
@@ -1,35 +1,33 @@
 import { Link, Stack, Typography } from "@mui/joy";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { MdOutlineFileDownload } from "react-icons/md";
 import { Link as RouterLink } from "react-router-dom";
 import tg from "../../assets/tg.png";
-import { FaGithub, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { FaArrowRightLong } from "react-icons/fa6";
 
 export const HomeInfoSection = () => {
   const socialMediaLinks = [
     {
       href: "https://github.com/thetiagogil",
-      icon: <FaGithub className="icon" size={25} />,
+      icon: <FaGithub className="icon" size={25} />
     },
     {
       href: "https://www.linkedin.com/in/thetiagogil/",
-      icon: <FaLinkedin className="icon" size={25} />,
+      icon: <FaLinkedin className="icon" size={25} />
     },
     {
       href: "https://www.facebook.com/thetiagogil/",
-      icon: <FaFacebook className="icon" size={25} />,
+      icon: <FaFacebook className="icon" size={25} />
     },
     {
       href: "https://www.instagram.com/thetiagogil/",
-      icon: <FaInstagram className="icon" size={25} />,
-    },
+      icon: <FaInstagram className="icon" size={25} />
+    }
   ];
 
   return (
     <Stack sx={{ maxWidth: { xs: "100%", lg: "600px" }, gap: 4 }}>
-      <Stack
-        component="section"
-        sx={{ textAlign: { xs: "center", lg: "left" } }}
-      >
+      <Stack component="section" sx={{ textAlign: { xs: "center", lg: "left" } }}>
         <Typography level="h1">TIAGO GIL</Typography>
         <Typography level="h3" sx={{ fontSize: { xs: "20px", lg: "24px" } }}>
           Web Developer & Master Architect
@@ -42,29 +40,36 @@ export const HomeInfoSection = () => {
 
       <Stack component="section">
         <Typography level="body-md" sx={{ textAlign: "justify" }}>
-          As a Junior Full Stack Web Developer with a Master's in architecture,
-          I leverage my problem-solving skills and attention to detail to
-          create user-friendly web applications. My architectural background
-          inspires a fusion between design thinking and analytical
-          problem-solving, enabling me to craft innovative solutions that merge
+          As a Junior Full Stack Web Developer with a Master's in architecture, I leverage my problem-solving skills and
+          attention to detail to create user-friendly web applications. My architectural background inspires a fusion
+          between design thinking and analytical problem-solving, enabling me to craft innovative solutions that merge
           creativity, technology, and data.
         </Typography>
       </Stack>
 
-      <Stack
-        component="section"
-        sx={{ alignItems: { xs: "center", lg: "baseline" } }}
-      >
+      <Stack component="section" sx={{ alignItems: { xs: "center", lg: "baseline", gap: 8 } }}>
         <Link component={RouterLink} to="/timeline" underline="none">
           <Typography
             level="body-md"
             endDecorator={<FaArrowRightLong size={12} />}
             sx={{
               transition: "0.3s",
-              "&:hover": { color: "primary.white" },
+              "&:hover": { color: "primary.white" }
             }}
           >
             Check out my timeline
+          </Typography>
+        </Link>
+        <Link href="/CV_TiagoGil.pdf" download="CV_TiagoGil.pdf" underline="none">
+          <Typography
+            level="body-md"
+            endDecorator={<MdOutlineFileDownload size={16} />}
+            sx={{
+              transition: "0.3s",
+              "&:hover": { color: "primary.white" }
+            }}
+          >
+            Download my resume
           </Typography>
         </Link>
       </Stack>
@@ -74,7 +79,7 @@ export const HomeInfoSection = () => {
         sx={{
           flexDirection: "row",
           gap: 2,
-          justifyContent: { xs: "center", lg: "left" },
+          justifyContent: { xs: "center", lg: "left" }
         }}
       >
         {socialMediaLinks.map((link, index) => (
@@ -86,8 +91,8 @@ export const HomeInfoSection = () => {
               color: "primary.white3",
               transition: "color 0.3s",
               "&:hover": {
-                color: "primary.white",
-              },
+                color: "primary.white"
+              }
             }}
           >
             {link.icon}
