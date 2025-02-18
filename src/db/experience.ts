22c6461126fd8f38a28ce8e1143d9bafed05722a
thetiagogil <talmeidagil@hotmail.com>
2025-02-18 15:17:53 +0000

    update db

--- src/db/experience.ts
+++ src/db/experience.ts
@@ -20,9 +20,9 @@ export const experience: DataModel[] = [
     name: "Full-stack Developer",
     place: "Talent Protocol",
     description:
-      "Worked in a small team, building several applications that utilize blockchain as a core technology and data source. My responsibilities included implementing Figma designs from scratch, integrating APIs, managing user feedback, conducting tests and bug fixes. I have contributed to key projects like Talent Passport, Build.top, Playground/API, and Builder.fi.",
+      "Built and maintained multiple web3 applications that utilize blockchain as a core technology and data source. My responsibilities included implementing Figma designs from scratch, integrating APIs, managing user feedback, conducting tests and bug fixes. I have contributed to key projects like Talent Passport, Build.top, Playground/API, and Builder.fi.",
     link: "https://www.talentprotocol.com/",
-    techs: ["TypeScript", "React", "Next.js", "Ruby on Rails", "Joy UI", "Supabase", "Notion"],
+    techs: ["React", "Next.js", "TypeScript", "Ruby on Rails", "Joy UI", "Supabase", "Notion"],
     dateStart: new Date("2024-03"),
     dateEnd: new Date("2024-08"),
     status: "completed"
@@ -32,9 +32,9 @@ export const experience: DataModel[] = [
     category: "experience",
     name: "Full-stack Developer",
     place: "Subvisual",
-    description: "",
+    description: "Built both web3 and web2 applications, using blockchain and PostgreSQL for secure and scalable solutions. Developed and deployed smart contracts using Solidity, ensuring transparency and real-time updates for a decentralized app.",
     link: "https://subvisual.com/",
-    techs: ["TypeScript", "React", "Solidity", "Joy UI", "Supabase", "Notion"],
+    techs: ["React","TypeScript", "Solidity", "Joy UI", "Supabase", "Notion"],
     dateStart: new Date("2024-12"),
     dateEnd: new Date("2025-02"),
     status: "completed"
--- src/db/projects.ts
+++ src/db/projects.ts
@@ -9,7 +9,7 @@ export const projects: DataModel[] = [
     description:
       "Towerdefense videogame where the players use math to win. Independently conceived and executed the game.",
     link: "https://giraffes-vs-sea.netlify.app/",
-    techs: ["HTML", "CSS", "JavaScript", "DOM Manipulation"],
+    techs: ["JavaScript", "HTML", "CSS", "DOM Manipulation"],
     dateStart: new Date("2023-09"),
     status: "completed",
     img: "../../../project-0.png"
@@ -22,7 +22,7 @@ export const projects: DataModel[] = [
     description:
       "Web application that gives information about the video game League of Legends, allowing users to create and customize builds.",
     link: "https://house-of-legends.netlify.app/",
-    techs: ["HTML", "CSS", "JavaScript", "React", "Node.js", "REST API"],
+    techs: ["React", "JavaScript", "CSS", "Node.js", "REST API"],
     dateStart: new Date("2023-10"),
     status: "outdated",
     img: "../../../project-1.png"
@@ -33,7 +33,7 @@ export const projects: DataModel[] = [
     name: "FIN/ACE",
     subject: "Personal Finances Website",
     link: "https://finace.netlify.app/",
-    techs: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "MongoDB"],
+    techs: ["React", "JavaScript", "CSS", "Node.js", "Express.js", "MongoDB"],
     dateStart: new Date("2023-11"),
     dateEnd: new Date("2023-12"),
     status: "outdated"
@@ -44,7 +44,7 @@ export const projects: DataModel[] = [
     name: "Personal website",
     subject: "Personal website",
     link: "https://thetiagogil.netlify.app/",
-    techs: ["HTML", "JavaScript", "React", "Joy UI"],
+    techs: ["React", "TypeScript", "Joy UI"],
     dateStart: new Date("2024-01"),
     status: "completed"
   },
@@ -56,7 +56,7 @@ export const projects: DataModel[] = [
     description:
       "Web application designed for financial tracking and analysis. It provides users with tools to manage and visualize their income and expenses over different years and months, as well as offering various features to help users understand their financial data through detailed summaries and visual representations.",
     link: "https://finace-v2.netlify.app/",
-    techs: ["HTML", "TypeScript", "React", "Node.js", "Express.js", "Supabase", "Joy UI"],
+    techs: ["React", "TypeScript", "Node.js", "Express.js", "Supabase", "Joy UI"],
     dateStart: new Date("2024-07"),
     dateEnd: new Date("2024-08"),
     img: "../../../project-4.png",
@@ -70,7 +70,7 @@ export const projects: DataModel[] = [
     description:
       "Web application designed for financial tracking and analysis. It provides users with tools to manage and visualize their income and expenses over different years and months, as well as offering various features to help users understand their financial data through detailed summaries and visual representations.",
     link: "",
-    techs: ["HTML", "TypeScript", "React", "Node.js", "Express.js", "Supabase", "Joy UI"],
+    techs: ["React", "TypeScript", "Node.js", "Express.js", "Supabase", "Joy UI"],
     dateStart: new Date("2024-09"),
     status: "open"
   }, */
@@ -81,7 +81,7 @@ export const projects: DataModel[] = [
     place: "Talent Protocol",
     subject: "",
     link: "https://passport.talentprotocol.com/",
-    techs: ["TypeScript", "React", "Joy UI"],
+    techs: ["React", "TypeScript", "Joy UI"],
     dateStart: new Date("2024-04"),
     dateEnd: new Date("2024-08"),
     status: "completed"
@@ -93,7 +93,7 @@ export const projects: DataModel[] = [
     place: "Talent Protocol",
     subject: "",
     link: "https://build.top/",
-    techs: ["TypeScript", "Next.js", "Joy UI"],
+    techs: ["Next.js", "TypeScript", "Joy UI"],
     dateStart: new Date("2024-04"),
     dateEnd: new Date("2024-06"),
     status: "completed"
@@ -105,7 +105,7 @@ export const projects: DataModel[] = [
     place: "Talent Protocol",
     subject: "",
     link: "https://mirror.xyz/talentprotocol.eth/dSkw_mNNOgs6GZEjZ3GTJMt0uJ5DjpWGkX3rncF1OD8/",
-    techs: ["TypeScript", "Next.js", "Joy UI"],
+    techs: ["Next.js", "TypeScript", "Joy UI"],
     dateStart: new Date("2024-04"),
     dateEnd: new Date("2024-08"),
     status: "inactive"
@@ -113,7 +113,7 @@ export const projects: DataModel[] = [
   {
     id: 9,
     category: "projects",
-    name: "EASYQA",
+    name: "EASYQA OS",
     subject: "Question and Answers Website",
     description:
       "Q&A app inspired by Builderfi by Talent Protocol. It offers a user-friendly platform for asking and answering questions, with a voting system, allowing users to engage with a community of learners and experts. The design is heavily inspired by Builderfi, ensuring a modern and intuitive interface that enhances user experience.",
